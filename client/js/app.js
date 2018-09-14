e3c94383793076ec865fb84fd7d9d4cea6458118
lbarret4 <llewellynbar4@gmail.com>
2018-09-14 15:09:33 -0400

    Complete mock functionalty of add, edit, and delete of chirp post

--- client/js/app.js
+++ client/js/app.js
@@ -1,45 +1,50 @@
 $( document ).ready(function () {
 
-//let $chirpText = $('#chirpText');
 let $post = $('#post');
 
 $post.on('click',(e) =>{
     e.preventDefault();
     let $chirpText = $('#chirpText');
     let chirp = $chirpText.val();
-    $('ul').append(`<li class="list-group-item list-group-item-action d-flex flex-column" data-toggle="modal" data-target="#chirpsModal"><div class="d-flex justify-content-between "><small class="text-muted  d-flex align-self-center ">User1:</small><button type="button" class="close " aria-label="Close"><span aria-hidden="true">&times;</span></button></div><p class="mx-3">${chirp}</p></li>`);
+    $('ul').append(`<li class="list-group-item list-group-item-action d-flex flex-column" data-toggle="modal" data-target="#chirpsModal"><div class="d-flex justify-content-between "><small class="text-muted  d-flex align-self-center ">User1:</small><button type="button" class="close deleteChirp" aria-label="Close" ><span aria-hidden="true">&times;</span></button></div><p class="mx-3">${chirp}</p></li>`);
+
+    $('.deleteChirp').on('click',(event) =>{
+        let $li= $(event.target).parent().parent();
+        let closeAttr ="data-toggle data-target";
+        $li.removeAttr(closeAttr);
+        $li.remove();  
+    });
     
 });
 
 
 //*Modal
 $('#chirpsModal').on('show.bs.modal', function (event) {
-    console.log(Event.relatedTarget);
-    var li = $(event.relatedTarget).children('p'); // Button that triggered the modal
-    var recipient = li.text(); // Extract info from data-* attributes
+
+    var $p = $(event.relatedTarget).children('p'); // Button that triggered the modal
+    var currentText = $p.text(); // Extract info from data-* attributes
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    console.log(li);
-    console.log(recipient);
+
     var modal = $(this);
-    modal.find('.modal-body textarea').val(recipient);
+    modal.find('.modal-body textarea').val(currentText);
 
     $("#save").on('click',(e) =>{
         e.preventDefault();
         let edit= modal.find('.modal-body textarea').val();
-        console.log(edit);
-        li.text(edit);
+
+        $p.text(edit);
     
       });
     
   });
 
-
-
-
-
-
-
+$('.deleteChirp').on('click',(event) =>{
+    let $li= $(event.target).parent().parent();
+    let closeAttr ="data-toggle data-target";
+    $li.removeAttr(closeAttr);
+    $li.remove();  
+});
 
 
 
