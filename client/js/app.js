217db0f34dd54f7ce3f40246e5fbc5d99ee176ed
lbarret4 <llewellynbar4@gmail.com>
2018-09-14 15:37:47 -0400

    fix multiple event listeners on close button

--- client/js/app.js
+++ client/js/app.js
@@ -2,13 +2,18 @@ $( document ).ready(function () {
 
 let $post = $('#post');
 
+// $.get();
+
+
+
 $post.on('click',(e) =>{
     e.preventDefault();
     let $chirpText = $('#chirpText');
     let chirp = $chirpText.val();
-    $('ul').append(`<li class="list-group-item list-group-item-action d-flex flex-column" data-toggle="modal" data-target="#chirpsModal"><div class="d-flex justify-content-between "><small class="text-muted  d-flex align-self-center ">User1:</small><button type="button" class="close deleteChirp" aria-label="Close" ><span aria-hidden="true">&times;</span></button></div><p class="mx-3">${chirp}</p></li>`);
+    $li=createChirp(chirp);
+    $closeBtn =$li.children().children('.deleteChirp');
 
-    $('.deleteChirp').on('click',(event) =>{
+    $closeBtn.on('click',(event) =>{
         let $li= $(event.target).parent().parent();
         let closeAttr ="data-toggle data-target";
         $li.removeAttr(closeAttr);
@@ -47,7 +52,11 @@ $('.deleteChirp').on('click',(event) =>{
 });
 
 
+function createChirp(text,user="User1"){
+    $li = $(`<li class="list-group-item list-group-item-action d-flex flex-column" data-toggle="modal" data-target="#chirpsModal"><div class="d-flex justify-content-between "><small class="text-muted  d-flex align-self-center ">${user}</small><button type="button" class="close deleteChirp" aria-label="Close" ><span aria-hidden="true">&times;</span></button></div><p class="mx-3">${text}</p></li>`).appendTo('ul');
+    return $li;
 
+};
 
 
 
