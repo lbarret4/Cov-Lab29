f2484c4f4479b763921fc1ddd18c730f0a059b89
lbarret4 <llewellynbar4@gmail.com>
2018-09-14 00:51:37 -0400

    setup files

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const PORT = 3000;
+
+let app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT);
+
+
+
+
